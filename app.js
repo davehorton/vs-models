7afc7b641622f61a1ab01e546a3e5880b1cf9461
Dave Horton <daveh@beachdognet.com>
2016-01-31 19:44:26 -0500

    add virtual to rdr

--- app.js
+++ app.js
@@ -2,15 +2,19 @@
 
 var _ = require('lodash') ;
 
+var paths = {} ;
 var models = {} ;
 
 require('fs').readdirSync(__dirname + '/lib/').forEach(function(file) {
   if (file.match(/\.js$/) !== null) {
     var modelName = _.capitalize( _.camelCase( file.split('.')[0] ) );
-    models[modelName] = './lib/' + file;
+    paths[modelName] = './lib/' + file;
   }
 });
 
 module.exports = function(mongoose, modelName) {
-  return require(models[modelName])(mongoose) ;
+  if( modelName in models ) {
+    return models[modelName] ;
+  }
+  return models[modelName] = require(paths[modelName])(mongoose) ;
 } ;
